43da059f0de2bde9df7dcf3127a8d4f02f0b873a
Micheal <mwmakarov@bk.ru>
2023-02-21 21:17:43 +0400

    добавлены кнопки навигации

---
+++ src/js/blocks/mainContentSlider.js
@@ -0,0 +1,48 @@
+export default function mainContentSlider() {
+    let currentSlide = 1;
+    // const slidesCount = document.querySelectorAll('.wrapper .content > *').length;
+    const slidesCount = 5;
+    const contentBlock = document.querySelector('.content');
+
+    const controlLeft = document.querySelector('.controls__button--left');
+    if (currentSlide === 1) {
+        controlLeft.style.opacity = '0.3';
+        controlLeft.style.pointerEvents = 'none';
+    }
+
+    const controlRight = document.querySelector('.controls__button--right');
+
+    function changeContentBlockTranslate(contentBlock, currentSlide) {
+        contentBlock.style.transform = `translateX(-${100 / slidesCount * (currentSlide - 1)}%)`;
+    }
+
+    controlLeft.addEventListener('click', () => {
+        if (currentSlide !== 1) {
+            currentSlide--;
+            changeContentBlockTranslate(contentBlock, currentSlide);  
+        }
+
+        if (currentSlide === 1) {
+            controlLeft.style.opacity = '0.3';
+            controlLeft.style.pointerEvents = 'none';
+        } else {
+            controlRight.style.opacity = '';
+            controlRight.style.pointerEvents = '';
+
+        }
+    })
+    controlRight.addEventListener('click', () => {
+        if (currentSlide !== slidesCount) {
+            currentSlide++;
+            changeContentBlockTranslate(contentBlock, currentSlide);
+        }
+
+        if (currentSlide === slidesCount) {
+            controlRight.style.opacity = '0.3';
+            controlRight.style.pointerEvents = 'none';
+        } else {
+            controlLeft.style.opacity = '';
+            controlLeft.style.pointerEvents = '';
+        }
+    })
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
+import mainContentSlider from "./blocks/mainContentSlider.js";
 import technologies from "./blocks/technologies.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     technologies();
+    mainContentSlider();
 })
